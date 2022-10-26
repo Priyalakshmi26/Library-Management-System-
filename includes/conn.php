0d24058e280b84b17e7e5fc2c99700c7c344e175
Priyalakshmi P.R <84332515+Priyalakshmi26@users.noreply.github.com>
2022-10-26 19:52:19 +0530

    Add files via upload

---
+++ includes/conn.php
@@ -0,0 +1,9 @@
+<?php
+	$conn = new mysqli('localhost', 'root', '', 'libsystem');
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+
+?>
+
+&^eR0r%d6Av87C/9
